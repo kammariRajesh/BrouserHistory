3acf4c2a948756df126d324d8e4b1007289b320d
kammariRajesh <k.rajeshravi366@gmail.com>
2022-09-22 08:12:52 +0000

    commit message

---
+++ src/components/BrouseItem/index.js
@@ -0,0 +1,35 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class BrouseItem extends Component {
+  deleteBrouseItem = () => {
+    const {item, deleteHistory} = this.props
+    const {id} = item
+    return deleteHistory(id)
+  }
+
+  render() {
+    const {item} = this.props
+    const {timeAccessed, logoUrl, title, domainUrl} = item
+    return (
+      <div className="list-item-bg-container">
+        <li className="list-item">
+          <p className="time">{timeAccessed}</p>
+          <img src={logoUrl} alt="domain logo" className="app-icon" />
+          <p className="title">{title}</p>
+          <p className="domain-url">{domainUrl}</p>
+        </li>
+        <button type="button" className="delete-icon" data-testid="delete">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+            onClick={this.deleteBrouseItem}
+          />
+        </button>
+      </div>
+    )
+  }
+}
+
+export default BrouseItem
